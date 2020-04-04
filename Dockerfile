7ef443bfd8acb0d6b8de4de7d77a8888e2f8cc7d
Manuel Jiménez <man.jime@gmail.com>
2020-04-04 23:56:50 +0000

    adicion

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
-FROM php:7-apache
-RUN docker-php-ext-install mysql
+FROM php:5.6-apache
+RUN apt-get -y update && apt-get upgrade -y
+RUN docker-php-ext-install mysql && docker-php-ext-enable mysql
--- www/courses/_config.php
+++ www/courses/_config.php
@@ -1,9 +1,8 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1); 
-
-define('HOSTNAME', 'localhost');
-define('USERNAME', 'activars');
-define('PASSWORD', '8htjfcdt9j2ga');
-define('DATABASE', 'activars_courses');
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+
+define('HOSTNAME', getenv('MYSQL_HOST'));
+define('USERNAME', getenv('MYSQL_USER'));
+define('PASSWORD', getenv('MYSQL_PASSWORD'));
+define('DATABASE', getenv('MYSQL_DATABASE'));
 ?>
